30916e11e5b68ef1ccc54d74a40f75eb37144575
taweel <taweel@yamsafer.me>
2019-10-09 17:36:33 +0300

    enhancements

--- test/challenges1.js
+++ test/challenges1.js
@@ -3,26 +3,26 @@ const challenge1 = require('../src/challenges/challenge1')
 
 const testData = [
   {
-    arr: [5, 8, -1, 7, 9],
+    input: [5, 8, -1, 7, 9],
     expect: [9, 7, -1, 8, 5]
   },
   {
-    arr: [4, 4, 4, 4],
+    input: [4, 4, 4, 4],
     expect: [4, 4, 4, 4]
   },
   {
-    arr: [3, 3, 9, 2],
+    input: [3, 3, 9, 2],
     expect: [2, 9, 3, 3]
   },
   {
-    arr: [9],
+    input: [9],
     expect: [9]
   }
 ]
 describe('Challenge 1 ->', function() {
   testData.forEach((data) => {
-    it(`should return ${data.expect} for [${data.arr}] `, function() {
-      assert.equal(challenge1.solution(data.arr), data.expect)
+    it(`should return ${data.expect} for [${data.input}] `, function() {
+      assert.equal(challenge1.solution(data.input), data.expect)
     })  
   })
 })
--- test/challenges2.js
+++ test/challenges2.js
@@ -3,26 +3,26 @@ const challenge1 = require('../src/challenges/challenge2')
 
 const testData = [
   {
-    arr: [5, 8, -1, 7, 9],
+    input: [5, 8, -1, 7, 9],
     expect: 9
   },
   {
-    arr: [4, 4, 4, 4],
+    input: [4, 4, 4, 4],
     expect: 4
   },
   {
-    arr: [3, 3, 9, 2],
+    input: [3, 3, 9, 2],
     expect: 9
   },
   {
-    arr: [9],
+    input: [9],
     expect: 9
   }
 ]
 describe('Challenge 2 ->', function() {
   testData.forEach((data) => {
-    it(`should return ${data.expect} for [${data.arr}] `, function() {
-      assert.equal(challenge1.solution(data.arr), data.expect)
+    it(`should return ${data.expect} for [${data.input}] `, function() {
+      assert.equal(challenge1.solution(data.input), data.expect)
     })  
   })
 })
