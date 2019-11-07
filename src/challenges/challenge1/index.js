bc2bf2fc65d16b761124c42cee10b87ac8892e3f
Noor-z <noor.a.m.zahran@gmail.com>
2019-11-07 11:08:43 +0200

    completed final solution

--- src/challenges/challenge1/index.js
+++ src/challenges/challenge1/index.js
@@ -1,6 +1,10 @@
 function solution(arr) {
-  //... REPLACE WITH YOUR CODE
-  return arr  
+  var ret = new Array;
+  for(var i = arr.length-1; i >= 0; i--) {
+      ret.push(arr[i]);
+  }
+  return (ret.slice(0, arr.length)).join(',');
+  //return ret; 
 }
 
 module.exports = {
--- src/challenges/challenge2/index.js
+++ src/challenges/challenge2/index.js
@@ -1,6 +1,12 @@
 function solution(arr) {
-  //... REPLACE WITH YOUR CODE
-  return arr  
+  var len = arr.length;
+  var max = -Infinity;
+  while (len--) {
+    if (arr[len] > max) {
+      max = arr[len];
+    }
+  }
+  return max;  
 }
 
 module.exports = {
