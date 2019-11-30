e7c112e17bd29c1de27062513af45af63ebab346
alaa frakhna <alaa.frakhna@hotmail.com>
2019-11-30 10:49:01 +0200

    test

--- src/challenges/challenge1/index.js
+++ src/challenges/challenge1/index.js
@@ -1,6 +1,13 @@
 function solution(arr) {
   //... REPLACE WITH YOUR CODE
-  return arr  
+    var arrlength = arr.length - 1;
+    for (i = 0; i <= arrlength / 2; i++) {
+        var x = arr[i];
+        var y = arr[arrlength - i];
+        arr[i] = y;
+        arr[arrlength - i] = x;
+    }
+    return arr
 }
 
 module.exports = {
