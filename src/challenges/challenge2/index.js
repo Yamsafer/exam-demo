84e6f318109e4b0810d32c0f6ac869db2542bf74
Mohammed Owda <oude.moh@gmail.com>
2019-12-30 22:59:38 +0200

    completed final solution

--- src/challenges/challenge2/index.js
+++ src/challenges/challenge2/index.js
@@ -1,6 +1,10 @@
 function solution(arr) {
-  //... REPLACE WITH YOUR CODE
-  return arr  
+  let great = arr[0]
+  for(let i=0;i< arr.length; i++){
+    if(arr[i] > great)
+    great = arr[i]
+  }
+  return great.toString(); 
 }
 
 module.exports = {
