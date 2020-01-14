a9a8b1b66b3fc924d5ec11adcf1ae2a6d2022ce6
Rand <rand.hazem03@hotmail.com>
2020-01-14 02:40:42 +0200

    solve chalange2 largest element

--- src/challenges/challenge2/index.js
+++ src/challenges/challenge2/index.js
@@ -1,8 +1,16 @@
 function solution(arr) {
-  //... REPLACE WITH YOUR CODE
-  return arr  
+    // since the array is not sorted, we can't use binary search with O(logn)
+    // so for that linear search will be sautable with O(n) time complexity
+
+    var max = arr[0]; // array range [1..1000] can't be empty
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max
 }
 
 module.exports = {
-  solution
+    solution
 }
