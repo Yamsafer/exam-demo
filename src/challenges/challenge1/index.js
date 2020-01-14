503fbaf0c7f458bf53fb7d7608cdf2a666fdb5fc
Rand <rand.hazem03@hotmail.com>
2020-01-14 02:29:13 +0200

    solve chalange1 array reverce

--- src/challenges/challenge1/index.js
+++ src/challenges/challenge1/index.js
@@ -1,8 +1,12 @@
 function solution(arr) {
-  //... REPLACE WITH YOUR CODE
-  return arr  
+    for (var i = 0; i < arr.length / 2; i++) {
+        var temp = arr[i];
+        arr[i] = arr[arr.length - i - 1];
+        arr[arr.length - i - 1] = temp;
+    }
+    return arr;
 }
 
 module.exports = {
-  solution
+    solution
 }
